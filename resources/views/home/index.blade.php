5b75143f2090addc35904a49234871465a6f5354
Celine <celinaqueenterosa@gmail.com>
2023-10-21 13:11:37 +0700

    Tugas Simasbe Celine Queenterosa Therik 12221361

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,13 @@
+@extends('home.app')
+@section('content')
+    <div class="px-4 py-5 text-center">
+        <img class="d-block mx-auto mb-4"
+            src="https://th.bing.com/th/id/R.5ace17ebf1a1a8e72829567eaee2ac62?rik=v1AaDDcH4p%2feCg&riu=http%3a%2f%2fstudents.bsi.ac.id%2fstudent%2fgambar%2flogo_ubsi.png&ehk=oGZVgJqmryvnKjEdZ9SgVVaCN7V3y%2b5giy9RD%2b868kE%3d&risl=&pid=ImgRaw&r=0"
+            alt="" width="100" height="100">
+        <h1 class="display-5 fw-bold text-body-emphasis">SIMASBE</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">Sistem Informasi Manajemen Arsip Surat Berbasis Elektronik</p>
+
+        </div>
+    </div>
+@endsection
